bf92b3a0b9a4874aab14b58aaef26bd7d8e53d01
susiloblogger <susiloblogger@gmail.com>
2026-01-01 06:41:42 +0700

    Updating packages

---
+++ src/contents/pages/pages.mjs
@@ -0,0 +1,50 @@
+export const pages = [
+  {
+    slug: 'tentang-kami',
+    title: 'Tentang Kami',
+    desc: 'lorem ipsum dolor sit amet consectetur adipisicing elit',
+    heading: 'About Us',
+  },
+  {
+    slug: 'jasa',
+    title: 'Layanan Jasa',
+    desc: 'lorem ipsum dolor sit amet consectetur adipisicing elit',
+    heading: 'About Us',
+  },
+  {
+    slug: 'portfolio',
+    title: 'Tentang Kami',
+    desc: 'lorem ipsum dolor sit amet consectetur adipisicing elit',
+    heading: 'Portfolio',
+  },
+  {
+    slug: 'testimoni',
+    title: 'Tentang Kami',
+    desc: 'lorem ipsum dolor sit amet consectetur adipisicing elit',
+    heading: 'About Us',
+  },
+  {
+    slug: 'kontak',
+    title: 'Tentang Kami',
+    desc: 'lorem ipsum dolor sit amet consectetur adipisicing elit',
+    heading: 'About Us',
+  },
+  {
+    slug: 'kemitraan',
+    title: 'Tentang Kami',
+    desc: 'lorem ipsum dolor sit amet consectetur adipisicing elit',
+    heading: 'About Us',
+  },
+  {
+    slug: 'kebijakan-privasi',
+    title: 'Kebijakan Privasi',
+    desc: 'Kami sangat menjaga privasi Anda. Halaman ini menjelaskan bagaimana kami mengumpulkan, menggunakan, dan melindungi informasi pribadi Anda.',
+    heading: 'Kebijakan Privasi Kami',
+  },
+  {
+    slug: 'ketentuan-layanan',
+    title: 'Ketentuan Layanan',
+    desc: 'Halaman ini menjelaskan syarat dan ketentuan penggunaan layanan kami. Dengan mengakses atau menggunakan layanan kami, Anda menyetujui ketentuan ini.',
+    heading: 'Ketentuan Layanan',
+  },
+];
---
+++ src/contents/services/approach.mjs
@@ -0,0 +1,26 @@
+export const approach = [
+  {
+    slug: 'website-development',
+    services: 'Website Development',
+  },
+  {
+    slug: 'it-consulting',
+    services: 'IT Consulting',
+  },
+  {
+    slug: 'data-system-migration',
+    services: 'Data & System Migration',
+  },
+  {
+    slug: 'cloud-hosting',
+    services: 'Cloud Hosting & Infrastructure',
+  },
+  {
+    slug: 'it-procurement-support',
+    services: 'IT Procurement & Support',
+  },
+  {
+    slug: 'saas-partners',
+    services: 'SaaS Partners',
+  },
+]
