f40f13ac555ad3c83a1395c0b22090d37c072df1
susiloblogger <susiloblogger@gmail.com>
2026-01-11 16:06:55 +0700

    Adding seo module

---
+++ src/lib/seos.ts
@@ -0,0 +1,10 @@
+export const SEOS = {
+  name: 'Ecofix',
+  title: 'Solusi Maintenance & Engineering Services di Jabodetabek | Ecofix',
+  image: 'https://ecofix.id/img/ogimage.png',
+  tagline: 'Efficient, Expert, Eco-Friendly',
+  description:
+    'Solusi paling lengkap untuk masalah saluran air mampet, grease trap, service ac, dan pengendalian hama. Layanan lebih cepat, profesional, dan bergaransi.',
+  url: import.meta.env.SITE_URL,
+  author: 'PT Gama Abyakta Sejahtera',
+};
