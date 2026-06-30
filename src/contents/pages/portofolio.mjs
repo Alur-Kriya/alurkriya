52d946cbb228302487d2979ac8073bd2c355ddd4
susiloblogger <susiloblogger@gmail.com>
2026-06-30 22:06:03 +0700

    updating portfolio page

--- src/contents/pages/portofolio.mjs
+++ src/contents/pages/portofolio.mjs
@@ -3,7 +3,7 @@ export const portfolio = [
     title: 'Software ZISWAF - Laznas DDII',
     description:
       'Software ini dibangun untuk kebutuhan pengelolaan zakat, infak, sedekah, wakaf, qurban, serta berbagai program kebaikan lainnya oleh Lembaga Amil Zakat Nasional Dewan Dawah Islamiyah Indonesia.',
-    image: 'https://images.unsplash.com/photo-1661956600655-e772b2b97db4',
+    image: '/images/portofolio/porto-a.png',
     tags: ['Laravel', 'NuxtJS', 'Bootstrap', 'NodeJS'],
     link: '#',
   },
@@ -11,7 +11,7 @@ export const portfolio = [
     title: 'Website Company Profile - Ecofix',
     description:
       'Website company profile yang dibangun khusus untuk perusahaan Ecofix, perusahaan yang berfokus pada maintenance services seperti, plumbing, grease trap, service ac di wilayah Jabodetabek.',
-    image: 'https://images.unsplash.com/photo-1661956600655-e772b2b97db4',
+    image: '/images/portofolio/porto-b.png',
     tags: ['Astro', 'Ghost', 'Tailwind', 'Firebase'],
     link: '#',
   },
@@ -19,7 +19,7 @@ export const portfolio = [
     title: 'Shipping Management System - Alena Kargo',
     description:
       'Software shipping management system yang dibangun khusus untuk perusahaan PT. Alena Mega Kargo, sebuah perusahaan ekspedisi layanan pengiriman barang dengan jumlah berat dan ukuran yang besar.',
-    image: 'https://images.unsplash.com/photo-1661956600655-e772b2b97db4',
+    image: '/images/portofolio/porto-c.png',
     tags: ['Figma', 'UI/UX', 'Tailwind', 'NodeJS'],
     link: '#',
   },
@@ -27,12 +27,12 @@ export const portfolio = [
     title: 'Website News Portal - Sabili',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
-    image: 'https://images.unsplash.com/photo-1661956600655-e772b2b97db4',
+    image: '/images/portofolio/porto-d.png',
     tags: ['Ghost', 'Cloudflare', 'NodeJS'],
     link: '#',
   },
   {
-    title: 'Website Company Profile - ID Adventure',
+    title: 'Situs Profil Perusahaan - ID Adventure',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
     image: 'https://images.unsplash.com/photo-1661956600655-e772b2b97db4',
@@ -40,10 +40,10 @@ export const portfolio = [
     link: '#',
   },
   {
-    title: 'Website Company Profile - Pesantren BIK',
+    title: 'Situs Profil Yayasan - Pesantren BIK',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
-    image: 'https://images.unsplash.com/photo-1661956600655-e772b2b97db4',
+    image: '/images/portofolio/porto-f.png',
     tags: ['Astro', 'Ghost', 'Vue.js', 'Firebase'],
     link: '#',
   },
